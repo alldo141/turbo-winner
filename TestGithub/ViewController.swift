7fb2c84befe79e3f31437860d0900e9c735a19f9
Alldo Kurniawan <alldo_k@yahoo.com>
2020-07-09 12:45:36 +0700

    Initial Commit

---
+++ TestGithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGithub
+//
+//  Created by Alldo Kurniawan on 09/07/20.
+//  Copyright © 2020 Alldo Kurniawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
